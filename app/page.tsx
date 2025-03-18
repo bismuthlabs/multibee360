70382dac833b1e7e332a550f1d534290a5ad9d5e
Bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2025-03-18 05:04:59 -0800

    really high credentials

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <OurServices />
       <WhyChooseUs />
       <RecentWorks />
-      {/* <TeamMembers /> */}
+      <TeamMembers />
       <Testimonials />
       <FAQSection />
       <Footer />
